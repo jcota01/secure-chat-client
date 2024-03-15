9bf2c789e757e8cde029fb999ca6ad23ceb2996e
CaptureTheFrog <capturethefrog@proton.me>
2024-03-15 11:13:01 +0000

    Edit to use keypair login with account file

--- login_ui.py
+++ login_ui.py
@@ -1,66 +1,64 @@
 import tkinter as tk
-from chat_ui import ChatWindow  # Import the ChatWindow we've defined earlier
+from tkinter import filedialog
+
+from typing import Tuple, Optional
+
+from Crypto.PublicKey import RSA
+
+import stubs
+from stubs import parse_account_file, login, register
+
 
 class LoginWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Secure Chat Login")
         self.geometry("300x200")
+        self.account: Optional[Tuple[str, RSA.RsaKey]] = None
 
-        self.username_label = tk.Label(self, text="Username")
-        self.username_label.pack()
-        self.username_entry = tk.Entry(self)
-        self.username_entry.pack()
-
-        self.password_label = tk.Label(self, text="Password")
-        self.password_label.pack()
-        self.password_entry = tk.Entry(self, show="*")
-        self.password_entry.pack()
-
-        self.login_button = tk.Button(self, text="Login", command=self.login)
-        self.login_button.pack()
+        self.file_picker = tk.Button(self, text="Login with Account File", command=self.pick_file)
+        self.file_picker.pack()
 
         self.register_button = tk.Button(self, text="Register", command=self.open_register_window)
         self.register_button.pack()
 
-    def login(self):
-        # This is where you would handle the actual login logic with the backend
-        username = self.username_entry.get()
-        password = self.password_entry.get()
-        # For now, we'll just open the chat window
-        self.open_chat_window()
+    def pick_file(self):
+        # Open a file picker dialog to choose the account file
+        filename = filedialog.askopenfilename()
+
+        try:
+            f = open('account', 'r')
+            self.account = parse_account_file(f.read())
+            f.close()
+            login(*self.account)
+            # if the server does not raise an exception login was successful
+            # exit login window thread
+            self.quit()
+        except BaseException:
+            # there was some error with login
+            print("Login error")
 
     def open_register_window(self):
         # Open the Register Window
         register_window = RegisterWindow()
         register_window.mainloop()
+        if register_window.account is not None:
+            # they registered ok
+            self.account = register_window.account
+            self.quit()
 
-    def open_chat_window(self):
-        # Open the Chat Window
-        self.destroy()  # Close the login window
-        chat_window = ChatWindow()
-        chat_window.mainloop()
 
 class RegisterWindow(tk.Toplevel):
     def __init__(self):
         super().__init__()
         self.title("Secure Chat Register")
-        self.geometry("300x250")
+        self.geometry("300x200")
+        self.account: Optional[Tuple[str, RSA.RsaKey]] = None
 
         self.username_label = tk.Label(self, text="Username")
         self.username_label.pack()
         self.username_entry = tk.Entry(self)
         self.username_entry.pack()
-
-        self.password_label = tk.Label(self, text="Password")
-        self.password_label.pack()
-        self.password_entry = tk.Entry(self, show="*")
-        self.password_entry.pack()
-
-        self.confirm_password_label = tk.Label(self, text="Confirm Password")
-        self.confirm_password_label.pack()
-        self.confirm_password_entry = tk.Entry(self, show="*")
-        self.confirm_password_entry.pack()
 
         self.register_button = tk.Button(self, text="Register", command=self.register)
         self.register_button.pack()
@@ -69,16 +67,22 @@ class RegisterWindow(tk.Toplevel):
         self.cancel_button.pack()
 
     def register(self):
-        # This is where you would handle the actual registration logic with the backend
         username = self.username_entry.get()
-        password = self.password_entry.get()
-        confirm_password = self.confirm_password_entry.get()
-        # For now, let's just print these values
-        print("Register with:", username, password, confirm_password)
-        # Ideally, after registration, you'd go to the chat window or back to the login
-        # self.destroy()  # Close the register window
-        # chat_window = ChatWindow()  # Open the chat window
-        # chat_window.mainloop()
+
+        print("Register with username:", username)
+        try:
+            self.account = register(username)
+            # assume registration success if no exception
+            print("Registered successfully")
+            # save the account file to the default place
+            f = open('account', 'w')
+            f.write(stubs.export_account_file(*self.account))
+        except BaseException:
+            # there was some error in registration
+            print("Failed to register, perhaps try a different username")
+
+        self.quit()
+
 
 if __name__ == "__main__":
     login_window = LoginWindow()
--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+import os
+from typing import Tuple, Optional
+
+from Crypto.PublicKey import RSA
+
 from login_ui import LoginWindow
+from chat_ui import ChatWindow
+from stubs import parse_account_file, login
+
 
 def main():
-    login_window = LoginWindow()
-    login_window.mainloop()
+    account: Optional[Tuple[str, RSA.RsaKey]] = None
+
+    # try to find login details in local storage
+    if os.path.isfile('account'):
+        try:
+            f = open('account', 'r')
+            account = parse_account_file(f.read())
+            f.close()
+            login(*account)
+            # if the server does not raise an exception login was successful
+        except BaseException:
+            # there was some error with login
+            account = None
+
+    if account is None:
+        login_window = LoginWindow()
+        login_window.mainloop()
+        account = login_window.account
+
+    if account is not None:
+        chat_window = ChatWindow()
+        chat_window.mainloop()
+
+
 
 if __name__ == "__main__":
     main()
