442149a60909557d5c6ec3ef7469ddf011b77301
CaptureTheFrog <capturethefrog@proton.me>
2024-03-19 00:44:39 +0000

    Add IP conversion utils

---
+++ utils/ip.py
@@ -0,0 +1,20 @@
+import re
+
+
+"""
+We use fixed32 to represent IPv4 addresses on the server as an IPv4 address is made of 4 octets (bytes).
+This allows for greater performance and scalability on the server, with only a little overhead in each client. 
+"""
+
+def ipv4_to_fixed32(ipv4: str) -> int:
+    assert re.fullmatch(
+        r"^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$",
+    ipv4)
+    octets = ipv4.split(".")
+    return (int(octets[0]) << (8 * 3)) | (int(octets[1]) << (8 * 2)) | (int(octets[2]) << (8 * 1)) | (
+                int(octets[3]) << (8 * 0))
+
+
+def fixed32_to_ipv4(fixed32: int) -> str:
+    assert 0 <= fixed32 <= 2 ** 32
+    return ".".join([str((fixed32 >> (8*3)) & 0xff), str((fixed32 >> (8*2)) & 0xff), str((fixed32 >> (8*1)) & 0xff), str((fixed32 >> (8*0)) & 0xff)])
