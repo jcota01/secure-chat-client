0ccb0e1b821a661cb64a704cd3990f609097356c
CaptureTheFrog <capturethefrog@proton.me>
2024-03-19 20:47:03 +0000

    Add known users model to db

--- database/models.py
+++ database/models.py
@@ -8,3 +8,9 @@ class Message(db.Model):
     from_user = db.Column(db.String(64), nullable=False)
     content = db.Column(db.LargeBinary, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
+class KnownUser(db.Model):
+    username = db.Column(db.String(64), primary_key=True, nullable=False)
+    chat_public_key = db.Column(db.LargeBinary, nullable=False)
+    last_known_address = db.Column(db.Integer, nullable=False)
