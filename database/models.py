fda85f0440e837a1d13c0f27f5c0253323dce9a4
CaptureTheFrog <capturethefrog@proton.me>
2024-03-19 19:23:55 +0000

    Remove unnecessary fields from db schema

--- database/models.py
+++ database/models.py
@@ -2,16 +2,9 @@ import datetime
 
 from utils.extensions import db
 
+
 class Message(db.Model):
-    __tablename__ = 'message'
     id = db.Column(db.Integer, primary_key=True)
-    from_user_id = db.Column(db.Integer, nullable=False)
-    to_user_id = db.Column(db.Integer, nullable=False)
-    text = db.Column(db.String(500), nullable=False)
-    # nonce = db.Column(db.LargeBinary)
-    # auth_tag = db.Column(db.LargeBinary)
-    # encrypted_pub_key = db.Column(db.LargeBinary)
-    nonce = db.Column(db.Text)
-    auth_tag = db.Column(db.Text)
-    encrypted_pub_key = db.Column(db.Text)
+    from_user = db.Column(db.String(64), nullable=False)
+    content = db.Column(db.LargeBinary, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
