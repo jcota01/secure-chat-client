1e1db3f160d1f037e4bc4c118e20b78083f57abf
CaptureTheFrog <capturethefrog@proton.me>
2024-03-19 23:53:11 +0000

    Add local message history

--- chat_ui.py
+++ chat_ui.py
@@ -1,8 +1,45 @@
 import tkinter as tk
-from tkinter import scrolledtext
+from datetime import datetime
+from typing import Optional
+
+import flask
+from sqlalchemy import asc
+
+from database.models import Message
+
 
 class ChatWindow(tk.Tk):
-    def __init__(self):
+    def __on_user_select(self, event):
+        w = event.widget
+        c = w.curselection()
+        if len(c) < 1:
+            return
+        index = int(c[0])
+        value = w.get(index)
+        self.switch_to_user(value)
+
+    @staticmethod
+    def __format_message(message: Message):
+        return f"[{message.timestamp.strftime('%H:%M:%S')}][{message.other_user if message.recipient_is_me else 'You'}] {message.content.decode('utf-8')}"
+
+    def switch_to_user(self, user: str):
+        if self.selected_user == user:
+            return
+
+        self.chat_display.configure(state=tk.NORMAL)
+        self.chat_display.delete(0, tk.END)
+        self.message_input.delete(0, tk.END)
+        self.message_input.insert(0, "Write your message here ....")
+        self.selected_user = user
+
+        # get past messages
+        with self.app.app_context():
+            messages = Message.query.filter_by(other_user=self.selected_user).order_by(asc(Message.timestamp)).limit(100).all()
+
+        for message in messages:
+            self.chat_display.insert(tk.END, ChatWindow.__format_message(message))
+
+    def __init__(self, app: flask.Flask):
         super().__init__()
         self.title("Secure Chat")
         self.geometry("600x500")  # Set the window size
@@ -17,6 +54,7 @@ class ChatWindow(tk.Tk):
         self.users_scrollbar = tk.Scrollbar(self.users_frame, orient=tk.VERTICAL)
         self.users_listbox = tk.Listbox(self.users_frame, activestyle="dotbox", bg='lightgrey',
                                         yscrollcommand=self.users_scrollbar.set)
+        self.users_listbox.bind('<<ListboxSelect>>', self.__on_user_select)
         self.users_scrollbar.config(command=self.users_listbox.yview)
         self.users_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.users_listbox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
@@ -24,9 +62,14 @@ class ChatWindow(tk.Tk):
         # Here you would have a method to update this list with users from the server
         
         # Main chat display area
-        self.chat_display = scrolledtext.ScrolledText(self, state='disabled')
-        self.chat_display.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
-        
+        self.chat_frame = tk.Frame(self)
+        self.chat_frame.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
+        self.chat_scrollbar = tk.Scrollbar(self.chat_frame, orient=tk.VERTICAL)
+        self.chat_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.chat_display = tk.Listbox(self.chat_frame, state='disabled', yscrollcommand=self.chat_scrollbar.set)
+        self.chat_display.pack(fill=tk.BOTH, expand=True)
+        self.chat_scrollbar.config(command=self.chat_display.yview)
+
         # Message input area
         self.message_frame = tk.Frame(self)
         self.message_frame.pack(fill=tk.X, side=tk.BOTTOM, padx=5, pady=5)
@@ -43,14 +86,24 @@ class ChatWindow(tk.Tk):
         self.send_button.pack(side=tk.RIGHT)
         
         self.protocol("WM_DELETE_WINDOW", self.on_close)  # Handle window closing
+        self.app = app
+        self.selected_user: Optional[str] = None
 
     def send_message(self):
-        # Logic to send a message
-        message = self.message_input.get()
+        # add message to local db
+        with self.app.app_context():
+            message = Message()
+            message.content = self.message_input.get().encode('utf-8')
+            message.timestamp = datetime.now()
+            message.recipient_is_me = False
+            message.other_user = self.selected_user
+            self.app.db.session.add(message)
+            self.app.db.session.commit()
+            self.app.db.session.refresh(message)
+        # send message to other user
+        pass
         # Add message to chat display
-        self.chat_display.configure(state='normal')
-        self.chat_display.insert(tk.END, message + '\n')
-        self.chat_display.configure(state='disabled')
+        self.chat_display.insert(tk.END, ChatWindow.__format_message(message))
         # Clear input field
         self.message_input.delete(0, tk.END)
 
--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import os
+import sys
+import threading
 from typing import Tuple, Optional
 
+import flask
+from sqlalchemy import desc
 from Crypto.PublicKey import RSA
 
 import ClientServerComms_pb2_grpc
+from database.models import KnownUser, Message
 from login_ui import LoginWindow
 from chat_ui import ChatWindow
 from utils.account import parse_account_file
 from account import *
+from utils.extensions import db
 
 
-def main():
+def create_app():
+    app = flask.Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret!'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+    app.db = db
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
+
+def run_app(app: flask.Flask):
+    app.run(host='0.0.0.0', port=6500)
+
+
+def main(app):
     account: Optional[Tuple[str, RSA.RsaKey, RSA.RsaKey]] = None
 
     # try to find login details in local storage
@@ -33,10 +58,17 @@ def main():
         account = login_window.account
 
     if account is not None:
-        chat_window = ChatWindow()
+        chat_window = ChatWindow(app)
+        with app.app_context():
+            users = KnownUser.query.join(Message, KnownUser.username == Message.other_user).group_by(KnownUser).order_by(desc(Message.timestamp)).all()
+        chat_window.update_users_list(u.username for u in users)
         chat_window.mainloop()
 
 
-
 if __name__ == "__main__":
-    main()
+    app = create_app()
+    t1 = threading.Thread(target=run_app, args=(app,))
+    t1.daemon = True
+    t1.start()
+    main(app)
+    sys.exit(0)
