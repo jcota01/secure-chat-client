0fd985c91ea4ff43b212b36e3796a9c43f7d0e15
CaptureTheFrog <capturethefrog@proton.me>
2024-03-19 02:48:39 +0000

    Read account from file

--- main.py
+++ main.py
@@ -11,15 +11,16 @@ from account import *
 
 
 def main():
-    account: Optional[Tuple[str, RSA.RsaKey]] = None
+    account: Optional[Tuple[str, RSA.RsaKey, RSA.RsaKey]] = None
 
     # try to find login details in local storage
     if os.path.isfile('account'):
         try:
             f = open('account', 'r')
-            username, keypair_login, keypair_chat = parse_account_file(f.read())
+            account = parse_account_file(f.read())
             f.close()
-            login(username, keypair_login)
+            login(account[0], account[1])
+
             # if the server does not raise an exception login was successful
         except BaseException:
             # there was some error with login
@@ -28,6 +29,7 @@ def main():
     if account is None:
         login_window = LoginWindow()
         login_window.mainloop()
+        login_window.quit()
         account = login_window.account
 
     if account is not None:
